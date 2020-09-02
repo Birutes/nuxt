2f21c2512d0c5f3238fb0d2b9efcb628e3165c2c
Birutes <birutepaplauskaite@yahoo.com>
2020-09-02 23:38:22 +0300

    nuxt

---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+        {
+          id: "balut",
+          title: "What is ballut?",
+          content: "Okay okay josefpoedofdsjfio"
+        },
+        {
+          id: "whereIsIt",
+          title: "Where is sign button?",
+          content: "snpseidknspdfjposdjfkjlkjdslkfnlkdsnfknsdklfnlks"
+        },
+        {
+          id: "how",
+          title: "How can I fix this problem?",
+          content: "smeofesdpfjikdsnpf"
+        }
+      ]
+})
